3698a992bd5de35ea7a06c51bed3b67e27eae091
Angela <angelacr@ymail.com>
2021-04-26 22:45:34 -0500

    readme, models, routes

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,23 +6,94 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+      .then(categoryDataDB => res.json(categoryDataDB))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+  })
+    .then(categoryDataDB => res.json(categoryDataDB))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+    .then(categoryDataDB => res.json(categoryDataDB))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(
+    {
+      category_name: req.body.category_name
+    },
+    {
+      where: {
+        id: req.params.id
+      }
+    })
+    .then(categoryDataDB => {
+      if (!categoryDataDB) {
+        res.status(404).json({ message: 'Sorry no category found with this id' });
+        return;
+      }
+      res.json(categoryDataDB);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+        id: req.params.id
+    }
+  })
+    .then(categoryDataDB => {
+        if (!categoryDataDB) {
+            res.status(404).json({ message: 'No category found with this id'});
+            return;
+        }
+        res.json(categoryDataDB);
+  })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
 
 module.exports = router;
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# e-commerce
+  # E-Commerce
+
+
+  ## Description
+  This application is the backend portion of an e-commerce website. 
+
+
+  # Table of Contents
+  * [Installation](#Installation)
+  * [Contributing](#Contributing)
+  * [Questions](#Questions)
+
+  ## Installation
+  Run 'npm i'
+
+  ## Contributing
+  Always room for people to contribute.
+
+  ## Questions
+  Contact me here: angelacr@ymail.com
+  <br>
+  <a href="github.com/neptune92" target="_blank">GitHub</a>
